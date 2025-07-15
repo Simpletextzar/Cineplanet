51d78c512daa6cdb61c86df08b20c76229328ed4
6miguel8 <mamezag@unjbg.edu.pe>
2025-07-15 05:47:35 -0500

    Listo, todo completo, solo falta limpiar y css

---
+++ crud/seleccionar_tabla.php
@@ -0,0 +1,19 @@
+<form action="listar_tabla.php" method="GET">
+    <label>Selecciona una tabla:</label>
+    <select name="tabla" required>
+        <option value="productos">Productos</option>
+        <option value="productos_tipos">Tipos de Productos</option>
+        <option value="salas_cine">Salas de Cine</option>
+        <option value="tipos_salas_cine">Tipos de Salas de Cine</option>
+        <option value="asientos">Asientos</option>
+        <option value="cines">Cines</option>
+        <option value="ciudades">Ciudades</option>
+        <option value="clientes">Clientes</option>
+        <option value="empleados">Empleados</option>
+        <option value="rol_empleados">Roles de los Empleados</option>
+        <option value="funciones">Funciones</option>
+        <option value="peliculas">Peliculas</option>
+        <option value="generos">Generos</option>
+    </select>
+    <input type="submit" value="Cargar Formulario">
+</form>
